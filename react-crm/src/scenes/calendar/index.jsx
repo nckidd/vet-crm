95ed57002938effc28249ff91407dd35d5d6b0a9
nckidd <christenekidd@gmail.com>
2024-08-19 08:14:24 -0800

    calendar and faq add

--- react-crm/src/scenes/calendar/index.jsx
+++ react-crm/src/scenes/calendar/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import FullCalendar, { formatDate } from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
+import { formatDate } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -25,7 +26,7 @@ const Calendar = () => {
         const calendarApi = selected.view.calendar;
         calendarApi.unselect();
 
-        if (title) {
+        if (title) { 
             calendarApi.addEvent({
                 id: `${selected.dateStr}-${title}`,
                 title,
@@ -57,11 +58,67 @@ const Calendar = () => {
             >
                 <Typography variant="h5">Events</Typography>
                 <List>
-                    
+                    {/* cycle through our current events */}
+                    {currentEvents.map((event) => (
+                        <ListItem
+                            key={event.id}
+                            sx={{ backgroundColor: colors.greenAccent[500],
+                            margin: "10px 0",
+                            borderRadius: "2px",
+                        }}
+                        >
+                            <ListItemText
+                                primary={event.title}
+                                secondary= {
+                                    <Typography>
+                                        {formatDate(event.start, {
+                                            year: "numeric",
+                                            month: "short",
+                                            day: "numeric",
+                                        })}
+                                    </Typography>
+                                }
+                            />
+                        </ListItem>
+                    ))}
                 </List>
+            </Box>
+
+            {/* CALENDAR */}
+            <Box 
+                flex="1 1 100%"
+                ml ="15px"
+            >
+                <FullCalendar
+                    height="75vh"
+                    plugins={[
+                        dayGridPlugin,
+                        timeGridPlugin,
+                        interactionPlugin,
+                        listPlugin
+                    ]}
+                    headerToolbar = {{
+                        left: "prev,next today",
+                        center: "title",
+                        right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+                    }}
+                    initialView="dayGridMonth"
+                    editable={true}
+                    selectable={true}
+                    selectMirror={true}
+                    dayMaxEvents={true}
+                    select={handleDateClick}
+                    eventClick={handleEventClick}
+                    eventsSet={(events) => setCurrentEvents(events)}
+                    initialEvents={[
+                        { id: "1234", title: "All-day event", date: "2024-08-29"},
+                        { id: "4321", title: "Timed event", date: "2024-08-30"},
+                    ]}
+                />
             </Box>
         </Box>
     </Box>
     );
 };
 
+export default Calendar;
