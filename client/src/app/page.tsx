b868164346238757b5c80b0d4e80c47acfba25ac
nckidd <christenekidd@gmail.com>
2024-09-02 11:20:54 -0800

    add dashboard and dashboard page

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Dashboard from "@/app/dashboard/page" //@ alias refers to the source directory
+                                              //prevents us from having to use a relative path
+export default function Home() {
+  return (
+    <Dashboard />
+  );
+}
