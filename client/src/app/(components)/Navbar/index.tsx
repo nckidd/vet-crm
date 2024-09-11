940c4918c2981413997bad641fc5c2fb23270cb7
nckidd <christenekidd@gmail.com>
2024-09-11 09:16:06 -0800

    add sidebar functionality

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -3,16 +3,29 @@
 
 import React from 'react'
 import { Bell, Link, Menu, Settings, Sun } from "lucide-react"
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsSidebarCollapsed } from '@/state';
 type Props = {}
 
 const Navbar = () => {
+  /* add state */
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+      (state) => state.global.isSidebarCollapsed
+  );
+  
+  /* toggle sidebar to invert isSidebarCollapsed */
+  const toggleSidebar = () => {
+      dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+  
   return (
     <div className="flex justify-between items-center w-full mb-7">
     {/* LEFT SIDE */}
     <div className="flex justify-between items-center gap-5">
       <button 
         className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue" 
-        onClick={() => {}}
+        onClick={toggleSidebar}
       >
         <Menu className="w-4 h-4"/>
       </button>
