65290193838a3fe1bfd08ccbbc8e68985a4acd50
nckidd <christenekidd@gmail.com>
2024-09-13 13:07:23 -0800

    add Navbar dark mode functionality

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -2,9 +2,9 @@
               //bc you cant pass onclick to any server component in nextjs
 
 import React from 'react'
-import { Bell, Link, Menu, Settings, Sun } from "lucide-react"
+import { Bell, Link, Menu, Moon, Settings, Sun } from "lucide-react"
 import { useAppDispatch, useAppSelector } from '@/app/redux';
-import { setIsSidebarCollapsed } from '@/state';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
 type Props = {}
 
 const Navbar = () => {
@@ -14,11 +14,18 @@ const Navbar = () => {
       (state) => state.global.isSidebarCollapsed
   );
   
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   /* toggle sidebar to invert isSidebarCollapsed */
   const toggleSidebar = () => {
       dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
   };
   
+  /* toggle dark mode to invert isDarkMode */
+  const toggleDarkMode = () => (
+    dispatch(setIsDarkMode(!isDarkMode))
+  );
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
     {/* LEFT SIDE */}
@@ -31,13 +38,17 @@ const Navbar = () => {
       </button>
     
     <div className='relative'>
-      <input type="search" placeholder="Start type to search groups & products" className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-whie rounded-lg focus:outline-none focus:border-blue-500" />
+      <input 
+        type="search" 
+        placeholder="Start type to search groups & products" 
+        className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-whie rounded-lg focus:outline-none focus:border-blue-500" />
       {/* bell */}
       <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
         <Bell className="text-gray-500" size={20} />
       </div>
     </div>
     </div>
+    
     {/* RIGHT SIDE */}
 
     <div className='flex justify-between items-center gap-5'>
@@ -45,8 +56,12 @@ const Navbar = () => {
       {/*hidden is default value and will be flex on larger screens*/}
       <div className='hidden md:flex justify-between items-center gap-5'>
         <div>
-          <button onClick={() => {}}>
-            <Sun className="cursor-pointer text-gray-500" size={24} />  
+          <button onClick={toggleDarkMode}>
+            {isDarkMode ? (
+              <Sun className="cursor-pointer text-gray-500" size={24} />
+            ) : (
+              <Moon className="cursor-pointer text-gray-500" size={24} />
+            )}
           </button>
         </div>
         <div className='relative'>
